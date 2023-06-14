896fe6dba207d74885600c72cc4b08503ad82d30
Andrew <mcdude22@gmail.com>
2023-06-14 10:21:21 -0700

    Added mouse event support

--- GameScreen.ts
+++ GameScreen.ts
@@ -3,11 +3,11 @@ import {Game} from './engine';
 export class GameScreen{
     game: Game;
     input = {};
-    
+
     constructor(game: Game) {
         this.game = game;
     }
-    
+
     inputHandler = (event: KeyboardEvent) => {
         if(event.type === 'keydown'){
             this.keyDown(event);
@@ -18,19 +18,38 @@ export class GameScreen{
             this.input[event.key] = false;
         }
     }
-    
+    mouseHandler = (event: MouseEvent) => {
+        console.log(event);
+        if(event.type === 'mousedown'){
+            this.mouseDown(event);
+            this.input["mouse"] = true;
+        }
+        if(event.type === 'mouseup'){
+            this.mouseUp(event);
+            this.input["mouse"] = false;
+        }
+    }
+
     // Define this method to create logic surrounding key down events
     keyDown(event: KeyboardEvent){}
     // Define this method to create logic surrounding key up events
     keyUp(event: KeyboardEvent){}
+    // Define this method to create logic surrounding mouse down events
+    mouseDown(event: MouseEvent){}
+    // Define this method to create logic surrounding mouse up events
+    mouseUp(event: MouseEvent){}
     readonly listener = e => this.inputHandler(e);
     show(){
         document.addEventListener('keydown', this.inputHandler);
         document.addEventListener('keyup', this.inputHandler);
+        document.addEventListener('mousedown', this.mouseHandler);
+        document.addEventListener('mouseup', this.mouseHandler);
     }
     update(deltaTime: number){};
     hide(){
         document.removeEventListener('keydown', this.inputHandler);
         document.removeEventListener('keyup', this.inputHandler);
+        document.removeEventListener('mousedown', this.mouseHandler);
+        document.removeEventListener('mouseup', this.mouseHandler);
     }
 }
